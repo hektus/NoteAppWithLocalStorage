1845a9349640effa7f982dfab5840f6f9748476b
Dawid Dzierzega <70227471+hektus@users.noreply.github.com>
2023-11-10 08:29:51 +0100

    add checked task function

--- main.js
+++ main.js
@@ -2,34 +2,44 @@ const input = document.querySelector("input");
 const addButton = document.querySelector(".save__button");
 const boxList = document.querySelector(".list__box");
 
-let taskIndex = 1;
+// let taskIndex = 1;
 
-function noteSave(e) {
+function saveNoteButton() {
   const newDiv = document.createElement("div");
   const text = input.value;
   if (text === "") return;
   newDiv.innerHTML = `
-  <p class="number">${taskIndex}. </p>
-  <p>${text}</p>
+  <p class="number"> </p>
+  <p class="text__input">${text}</p>
   <button class="delete">Usuń</button>
   <button class="checked">V</button>
 `;
 
   boxList.appendChild(newDiv);
-  //   newDiv.classList.add("task");
   input.value = "";
-  taskIndex++;
+  // taskIndex++;
+
+  //checked task
+  const doneButton = newDiv.querySelector(".checked");
+  doneButton.addEventListener("click", doneNoteButton);
+
+  //delete task
   const deleteButton = newDiv.querySelector(".delete");
-  deleteButton.addEventListener("click", deleteNote);
+  deleteButton.addEventListener("click", deleteNoteButton);
 }
 
-function deleteNote(e) {
+function deleteNoteButton(e) {
   const div = e.target.parentNode;
   boxList.removeChild(div);
-  //   const deleteButton = e.target;
-  //   console.log(deleteButton);
-  //   const taskDiv = deleteButton.closest(".task");
-  //   boxList.removeChild(taskDiv);
+
+  //initial function recalculating index task
+  // recaltulatingIndexTask();
+  // boxList.childElementCount;
 }
 
-addButton.addEventListener("click", noteSave);
+function doneNoteButton(e) {
+  const pText = e.target.parentNode.querySelector("p.text__input");
+  pText.classList.toggle("done");
+}
+
+addButton.addEventListener("click", saveNoteButton);
